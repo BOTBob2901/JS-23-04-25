fb36ab0d3ec6ec5281552cd69dcff0ff29bd3c3c
Igor Nikonov <128256749+BOTBob2901@users.noreply.github.com>
2025-04-23 15:16:08 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,105 @@
+"use strict";
+//Task#1
+// let name = 'Mary '
+// let activity = 'drink tea'
+// let bio = 'Our guest ' + name + activity + '.'
+// console.log(bio.length)
+
+
+//Task#2
+// const str = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+// let index = prompt("Enter a number");
+// alert(str.charAt(index))
+
+
+//Task#3
+// const str1 = prompt ("Enter a string")
+// alert(str1[str1.length-2]+str1[str1.length-1])
+
+
+//Task#4
+// const str2 = 'abcde'
+// let str3 =''
+// for(let i=str2.length-1 ; i>=0 ; i--)
+//   str3 += str2[i];
+// alert(str3)
+
+
+//Task#5
+// let str4 = 'js'
+// alert(str4.toUpperCase())
+
+
+//Task#6
+// const str = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+// const str5 = prompt("Enter a string");
+// alert(str5.includes(str5))
+
+
+//Task#7
+// let str6 = '1-2-3-4-5'
+// const element = '-'
+// while(str6.includes(element))
+//   str6 = str6.replace(element,'.')
+// alert(str6)
+
+
+//Task#8
+// let str6 = '1-2-3-4-5'
+// alert(str6.replaceAll('-','.'))
+
+
+//Task#9
+// const str7 = 'sdfwd'
+// console.log(str7.substring(4, 2))// fw
+
+
+//Task#10
+// const str10 = "I'm learning javascript!";
+// console.log(str.slice(4,12))
+// console.log(str.slice(13,23))
+// console.log(str.substring(4,12))
+// console.log(str.substring(13,23))
+// console.log(str.substr(4,8))
+// console.log(str.substr(13,10))
+
+
+//Task#11
+// const str11 = 'http://www.notion.so/String-820cf1e906884d8fb9c02236fe1c6dcb/'
+// alert(str11.indexOf('http://') === 0 ? 'Yes' : 'No')
+
+
+//Task#12
+// const str12 = 'https://www.notion.html'
+// alert(str12.indexOf('.html', str12.length-5) != -1 ? 'Yes' : 'No')
+
+
+//Task#13
+// const str13 = 'ab cd cd cd ef';
+// console.log(str.lastIndexOf('cd')); //9
+// console.log(str.lastIndexOf('cd', 8)); //6
+
+
+//Task#14
+// const str = 'abcde';
+// console.log(str.startsWith('abc')); //True
+// console.log(str.startsWith('xxx')); //False
+// console.log(str.startsWith('bc', 1)); //True
+
+
+//Task#15
+// const str = 'abcde';
+// console.log(str.endsWith('cde')); //True
+// console.log(str.endsWith('xxx')); //False
+// console.log(str.endsWith('bc', 3)); //True
+
+
+// //Task#16
+// let str = 'ab-cd-ef';
+// console.log(str.split('-')); // [ab,cd,ef]
+// console.log(str.split('-', 2)); // [ab,cd]
+// console.log(str.split('')); // [a,b,-,c,d,-,e,f]
+// console.log(str.split('', 3)); // [a,b,-]
+
+// let str2 = '12345';
+// console.log(str2.split('')); //[1,2,3,4,5]
